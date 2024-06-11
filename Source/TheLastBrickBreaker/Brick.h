74a1dc76e660f52bff5ea6da3192950bd9467f4e
David <djhatvr@gmail.com>
2024-06-11 16:58:32 -0700

    Add Brick Blueprint

---
+++ Source/TheLastBrickBreaker/Brick.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Brick.generated.h"
+
+UCLASS()
+class THELASTBRICKBREAKER_API ABrick : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABrick();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, Category = "Base Properties")
+	class UBoxComponent* BoxCollider = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "Base Properties")
+	class UStaticMeshComponent* BrickMesh = nullptr;
+};
