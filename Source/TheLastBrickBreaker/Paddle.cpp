d54e6fae63804db7724e302c81ebeed32d103fed
Primrose Jameson <kaibur17@gmail.com>
2024-06-10 23:51:22 -0400

    Added to Paddle.cpp
    Added collider and mesh components to Paddle class.

---
+++ Source/TheLastBrickBreaker/Paddle.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Paddle.h"
+#include "Components/CapsuleComponent.h"
+
+
+// Sets default values
+APaddle::APaddle()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleCollider = CreateDefaultSubobject<UCapsuleComponent>(FName("Capsule Collider"));
+	RootComponent = CapsuleCollider;
+
+	PaddleMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Paddle Mesh"));
+	PaddleMesh->SetupAttachment(CapsuleCollider);
+
+}
+
+// Called when the game starts or when spawned
+void APaddle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APaddle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APaddle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
